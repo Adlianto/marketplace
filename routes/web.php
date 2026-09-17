2be6387a9654189bfa3b7fb8aecde40ac19e781d
Bell <widiblamblaem@gmail.com>
2026-09-17 12:53:23 +0700

    fix(routes): rename profile update route to prevent collision with starter kit

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
 
     // Route Profile & Biodata
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.biodata.update');
 
     // Route Custom Avatar, Password & PIN
     Route::post('/profile/avatar', [ProfileController::class, 'updateAvatar'])->name('profile.avatar');
